1db8250e205debc218f538fd17b646681c341cc7
Michael Freeman <m.mfreeman@gmail.com>
2012-02-15 23:33:50 +0000

    mpo1a l2e2 update

--- MPO1A/L2E2Ascript.r
+++ MPO1A/L2E2Ascript.r
@@ -11,7 +11,6 @@
  #install.packages("rugarch")
  #install.packages("forecast")
  #install.packages("FinTS")
- #install.packages("rugarch")
  library(TSA) ; library(dynlm) ; library(rugarch) ; library(forecast) ; library(FinTS)
  
  
@@ -27,19 +26,19 @@
 
 # - [ Recall our best ARIMA model ] -
  arima1.14 <- arima(lppi.ts, order=c(1,1,4), fixed=c(NA, NA, 0, 0, NA)) ; coeftest(arima1.14)
- ArchTest(c(main$res), lags=5) # suffers from conditional heteroskedasticity
+ ArchTest(c(arima1.14$res), lags=5) # suffers from conditional heteroskedasticity
  par(mfrow=c(2,1)); acf(arima1.14$res^2); pacf(arima1.14$res^2)
- # -> spikes in ACF/PACF at 5 suggests we should try an ARCH 5 model
+ # -> spikes in ACF/PACF at 4 suggests we should try an ARCH 4 model
 
 # - [ Try fitting an ARCH(q) model ] -
- spec <- ugarchspec(variance.model = list(model="fGARCH", submodel="GARCH", garchOrder=c(5,0)),
+ spec <- ugarchspec(variance.model = list(model="fGARCH", submodel="GARCH", garchOrder=c(4,0)),
                     mean.model = list(armaOrder=c(1,4), include.mean=T),
                     fixed.pars=list(ma2=0,ma3=0))
- arch5 <- ugarchfit(data=lppi.d1, spec=spec)
- arch5
+ arch4 <- ugarchfit(data=lppi.d1, spec=spec)
+ arch4
  # mean is E[Y_t] (if greater than 0 => there is a mean intercept)
  # omega is the variance intercept
- plot(arch5, which="all")
+ plot(arch4, which="all")
 
 # - [ ARCH Diagnostics ] -
 
@@ -48,9 +47,9 @@
  # iii) If satisfy (i) and (ii), can we reduce the value of q?
  # iv)  If do not satisfy (i) and (ii) could try increasing q OR try GARCH
 
- nNA <- which(is.na(arch5@fit$resid)==F)
- arch5.sresid <- arch5@fit$residuals / arch5@fit$sigma
- ljung.box.test.1(arch5.sresid[nNA], seq(0,50,5))
+ nNA <- which(is.na(arch4@fit$resid)==F)
+ arch4.sresid <- arch4@fit$residuals / arch4@fit$sigma
+ ljung.box.test.1(arch4.sresid[nNA], seq(0,50,5))
  # -> q-stats seem OK
  # -> ARCH-LM in model output shows there exists remaining ARCH effects
  
@@ -77,6 +76,15 @@
  # -> q-stats seem OK
  # -> ARCH-LM in model output shows there aren't any remaining ARCH effects
  
+ # - [ Test residuals for normality ] -
+ par(mfrow=c(1,1))
+ hist(garch1.sresid, breaks=10, probability=T)
+ grid.x <- seq(min(garch1.sresid), max(garch1.sresid), length.out=1000)
+ grid.y <- dnorm(grid.x, sd=sd(garch1.sresid,na.rm=T))
+ lines(grid.x,grid.y)
+ jarque.bera.test(garch1.sresid[nNA])
+ # -> evidence of deviation from normality
+ 
 ## PART 3 : Other Models
 ## =================================================
  
@@ -85,6 +93,7 @@
  spec <- ugarchspec(variance.model = list(model="fGARCH", submodel="GARCH", garchOrder=c(1,1)),
                     mean.model = list(armaOrder=c(1,4), include.mean=F),
                     fixed.pars=list(ma2=0,ma3=0))
+ # -> not appropriate here since mean (mu) term is statistically significant
  # NOTE: you could do this in any of the models that follow too
  
 # - [ IGARCH ] -
@@ -92,6 +101,7 @@
  spec <- ugarchspec(variance.model = list(model="iGARCH", garchOrder=c(1,1)),
                     mean.model = list(armaOrder=c(1,4), include.mean=T),
                     fixed.pars=list(ma2=0,ma3=0))
+ tail(garch1@fit$coef,2) ; sum(tail(garch1@fit$coef,2))
  # -> not appropriate here
  
 # - [ TARCH ] -
@@ -124,13 +134,30 @@
                     fixed.pars=list(ma2=0,ma3=0))
  fit <- ugarchfit(data=lppi.d1, spec=spec, out.sample=20)
  # -> out.sample means to produce the forecast leaving out the last x (here 20) data points
-
+ # -> this will produce a pseudo-out-of-sample forecast
+ 
 # - [ Produce a forecast ] -
  forc <- ugarchforecast(fit, n.ahead=20)
+ forc.roll <- ugarchforecast(fit, n.ahead=20, n.roll=20)
+ # -> rolling forecast creates a new forecast as data becomes available
  forc
+ forc.roll@forecast$forecasts
+ 
+# - [ Plot the forecast ] -
+ plot(forc,which=1) # non-rolling forecast of series
+ plot(forc.roll,which=2) # rolling forecast of series
+ plot(forc,which=3) # forecast of sigma
 
-# - [ Explore the accuracy of the forecast a forecast ] -
- fpm(forc)
+# - [ Explore the accuracy of the forecast ] -
+ fpm(forc) # accuracy of non-rolling forecast
+ which.min(fpm(forc.roll)[1,])
+ which.min(fpm(forc.roll)[2,])
+ # -> forecast will 1-roll has lowest MSE and MAE
+ 
+# - [ Can also forecast using the full data set ] -
+ fit <- ugarchfit(data=lppi.d1, spec=spec)
+ forc <- ugarchforecast(fit, n.ahead=20)
+ par(mfrow=c(2,1));plot(forc,which=1);plot(forc,which=3)
  
  
 ## ===============================================
